3e818f644ab283ad5674b4ec4a265fbd3f6c7d83
Mauro Sanchez <Maurosan808@gmail.com>
2026-03-03 18:09:45 -0300

    remove base config for vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./",   // ESTA LÍNEA ES LA QUE FALTABA
+  plugins: [react()]
 })
